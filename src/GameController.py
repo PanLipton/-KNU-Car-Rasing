ec6112487fd785014a2495a2c2e0e287933e7582
PanLipton <panskillsasda@gmail.com>
2024-02-29 14:56:36 +0200

    Fix bugs x2

--- src/GameController.py
+++ src/GameController.py
@@ -2,13 +2,16 @@ import pygame
 import pygame_gui
 from Menu.MainMenu import MainMenu
 from MenuManager import MenuManager
-from config import BACKGROUND_IMAGE_PATH 
+from config import BACKGROUND_IMAGE_PATH
+from SoundManager.SoundManager import sound_manager
+
 
 class GameController:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((1366, 900))
         pygame.display.set_caption('Car Racing')
+        sound_manager.playMusicMenu()
         self.gui_manager = pygame_gui.UIManager((1366, 900), 'theme.json')
         self.menu_manager = MenuManager(self.screen, self.gui_manager, BACKGROUND_IMAGE_PATH)  # Передача шляху до зображення
         self.menu_manager.set_menu(MainMenu)  # Видалено зайвий аргумент                
--- src/Menu/SettingsMenu.py
+++ src/Menu/SettingsMenu.py
@@ -1,42 +1,48 @@
-# SettingsMenu.py
-
 import pygame
 import pygame_gui
 from Menu.BaseMenu import BaseMenu
-from pathlib import Path
+from SoundManager.SoundManager import sound_manager
+
 
 class SettingsMenu(BaseMenu):
     def __init__(self, screen, gui_manager, menu_manager, background_image_path):
-        title_font = pygame.font.Font(Path('../assets/fonts/pixel_font.ttf'), 100)  # Задаємо шрифт для заголовку
+        title_font = pygame.font.Font('../assets/fonts/pixel_font.ttf', 100)
         super().__init__(screen, gui_manager, menu_manager, background_image_path, "Car Racing", title_font)
+        self.slider_position = 0.5  # Значення за замовчуванням
         self.create_ui_elements()
 
     def create_ui_elements(self):
-        # Створення мітки "Volume"
         self.volume_label = pygame_gui.elements.UILabel(
-            relative_rect=pygame.Rect((self.screen.get_width() // 2 - 100, self.screen.get_height() // 2 - 80), (200, 30)),
+            relative_rect=pygame.Rect((self.screen.get_width() // 2 - 100, self.screen.get_height() // 2 - 120), (200, 30)),
             text='Volume',
             manager=self.gui_manager,
             container=self.gui_manager.get_root_container(),
             object_id='no_border_label'
         )
 
-        # Створення слайдера гучності
+        slider_width = 200
+        slider_height = 20
+        slider_x = (self.screen.get_width() - slider_width) // 2
+        slider_y = self.screen.get_height() // 2 - slider_height // 2 - 20  # Змінено положення по Y
+
         self.volume_slider = pygame_gui.elements.UIHorizontalSlider(
-            relative_rect=pygame.Rect((self.screen.get_width() // 2 - 100, self.screen.get_height() // 2 - 40), (200, 20)),
-            start_value=50,
-            value_range=(0, 100),
+            relative_rect=pygame.Rect((slider_x, slider_y), (slider_width, slider_height)),
+            start_value=self.slider_position,
+            value_range=(0.0, 1.0),
             manager=self.gui_manager,
-            container=self.gui_manager.get_root_container(),
-            object_id='no_border_slider'
+            container=self.gui_manager.get_root_container()
         )
 
-        # Створення кнопки "Back" за допомогою методу add_button
         self.add_button('Back', (self.screen.get_width() // 2 - 100, self.screen.get_height() // 2), self.back_action, 'no_border_button')
 
     def back_action(self):
         from Menu.MainMenu import MainMenu
+        self.slider_position = self.volume_slider.get_current_value()  # Зберігаємо поточне положення повзунка
         self.menu_manager.change_menu(MainMenu)
+
+    def update_volume(self):
+        volume = int(self.volume_slider.get_current_value() * 100)
+        sound_manager.setMusicMenuVolume(volume / 100)
 
     def draw(self):
         super().draw()
@@ -45,5 +51,4 @@ class SettingsMenu(BaseMenu):
     def handle_event(self, event):
         super().handle_event(event)
         if event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
-            if event.ui_element == self.volume_slider:
-                print(f"Volume set to {event.value}")
+            self.update_volume()
