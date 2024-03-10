8d897c75080a497f9ea23555fdbb9a866ba55794
LodinBohdan <tba1312@gmail.com>
2024-03-11 01:13:18 +0200

    Update GameOverScreen

--- src/GameOverScreen.py
+++ src/GameOverScreen.py
@@ -6,30 +6,39 @@ from SoundManager.SoundManager import sound_manager
 
 class GameOverScreen:
     _players = []
-    def __init__(self, screen, background_image_path, players):
+    def __init__(self, screen, game_over, players):
         self.screen = screen
-        self.background_image = pygame.image.load(background_image_path).convert()
-        self.background_image = pygame.transform.scale(self.background_image, (
+        self.game_over = pygame.image.load("../assets/img/gameoverimg.jpg").convert()
+        self.game_over = pygame.transform.scale(self.game_over, (
         screen.get_width(), screen.get_height()))  # Масштабирование изображения
         self._players = players
         self.font = pygame.font.Font(None, 36)
         self.font_large = pygame.font.Font(None, 72)
-        title_font = pygame.font.Font(('../assets/fonts/pixel_font.ttf'), 100)
-        self.text_color = (255, 255, 255)
+        #title_font = pygame.font.Font(('../assets/fonts/pixel_font.ttf'), 100)
+        pixel_font_path = "../assets/fonts/pixel_font.ttf"
+
+        self.font = pygame.font.Font(pixel_font_path, 30)  # Используйте пиксельный шрифт для маленького текста
+        self.font_large = pygame.font.Font(pixel_font_path, 48)  # Используйте пиксе
+        #self.text_color_red = (0, 0, 102)
+        self.text_color_red_black = (0, 0, 0)
         self.background_color = (0, 0, 0)
-        self.main_menu_text = self.font.render("Press SPACE to return to Main Menu", True, self.text_color)
-        self.game_over_text = self.font_large.render("Game Over", True, self.text_color)
-        self.game_over_text_rect = self.game_over_text.get_rect(center=self.screen.get_rect().center)
-        self.main_menu_text_rect = self.main_menu_text.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2 + 100))
+        #self.main_menu_text = self.font.render("Press SPACE to return to Main Menu", True, self.text_color_red)
+        self.main_menu_text = pygame.font.Font('../assets/fonts/pixel_font.ttf', 48).render(
+            "Press SPACE to return to Main Menu", True, (255, 255, 255))  # Увеличьте размер шрифта до 48
+
+
+        #self.game_over_text = self.font_large.render("Game Over", True, self.text_color)
+        #self.game_over_text_rect = self.game_over_text.get_rect(center=self.screen.get_rect().center)
+        self.main_menu_text_rect = self.main_menu_text.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2 + 115))
 
     def display_scores(self):
         players_dict = {}
         for i,player in enumerate(self._players):
-            players_dict[player.get_score()] = score_text = self.font.render(f"Player {i + 1}: {player.get_score()}", True, self.text_color)
+            players_dict[player.get_score()] = score_text = self.font.render(f"Player {i + 1}: {player.get_score()}", True, self.text_color_red_black)
 
         sorted_dict = dict(sorted(players_dict.items(), key=lambda item: item[0], reverse=True))
 
-        y_offset = 50  # Initial y offset
+        y_offset = 300  # Initial y offset
         for score, score_text in sorted_dict.items():
             score_text_rect = score_text.get_rect(center=(self.screen.get_width() // 2, y_offset))
             self.screen.blit(score_text, score_text_rect)
@@ -51,10 +60,18 @@ class GameOverScreen:
                         game_cont = GameController()
                         game_cont.run()
 
+            outline_text = pygame.font.Font('../assets/fonts/pixel_font.ttf', 48).render(
+                "Press SPACE to return to Main Menu", True, (0, 0, 0))
+            outline_text_rect = outline_text.get_rect(
+                center=(self.screen.get_width() // 2 + 2, self.screen.get_height() // 2 + 52))  # Смещение на 2 пикселя
+
+            self.main_menu_text_rect = self.main_menu_text.get_rect(
+                center=(self.screen.get_width() // 2, self.screen.get_height() // 2 + 55))
             #self.screen.fill(self.background_color)
-            self.screen.blit(self.background_image, (0, 0))  # Отображение заднего фона
+            self.screen.blit(self.game_over, (0, 0))  # Отображение заднего фона
             self.display_scores()
-            self.screen.blit(self.game_over_text, self.game_over_text_rect)
+            #self.screen.blit(self.game_over_text, self.game_over_text_rect)
+            self.screen.blit(outline_text, outline_text_rect)
             self.screen.blit(self.main_menu_text, self.main_menu_text_rect)
 
             pygame.display.flip()
