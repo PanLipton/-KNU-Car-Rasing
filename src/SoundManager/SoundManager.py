8f934e961289590f73a0b444d081a7808b5fc45a
PanLipton <panskillsasda@gmail.com>
2024-04-09 00:11:11 +0300

    update soundmanager for Github Actions

--- src/SoundManager/SoundManager.py
+++ src/SoundManager/SoundManager.py
@@ -1,6 +1,7 @@
 import pygame
 from pathlib import Path
 import struct
+
 class Sound:
     def __init__(self, file_path):
         self.sound = pygame.mixer.Sound(file_path)
@@ -10,7 +11,6 @@ class Sound:
 
     def set_volume(self, volume):
         self.sound.set_volume(volume)
-
 
 class SoundManager:
     _instance = None
@@ -24,51 +24,60 @@ class SoundManager:
     def __init__(self):
         if self._initialized:
             return
+
         self._initialized = True
-        pygame.mixer.init()
+        self.sound_enabled = True
 
-        # Звуки для різних подій
-        self.music_menu = Sound(Path('../assets/music/musicMenu.mp3'))
-        self.music_game = Sound(Path('../assets/music/musicGame.mp3'))
-        self.sound_button = Sound(Path('../assets/music/soundButton.mp3'))
-        self.sound_crash = Sound(Path('../assets/music/soundCollision.mp3'))
-        self.sound_win = Sound(Path('../assets/music/soundWin.mp3'))
-        self.sound_lose = Sound(Path('../assets/music/soundLose.mp3'))
-
+        try:
+            pygame.mixer.init()
+        except pygame.error:
+            print("No audio device found. Sound will be disabled.")
+            self.sound_enabled = False
+        else:
+            # Звуки для різних подій
+            self.music_menu = Sound(Path('../assets/music/musicMenu.mp3'))
+            self.music_game = Sound(Path('../assets/music/musicGame.mp3'))
+            self.sound_button = Sound(Path('../assets/music/soundButton.mp3'))
+            self.sound_crash = Sound(Path('../assets/music/soundCollision.mp3'))
+            self.sound_win = Sound(Path('../assets/music/soundWin.mp3'))
+            self.sound_lose = Sound(Path('../assets/music/soundLose.mp3'))
 
     def playMusicMenu(self):
-        self.stop_all()
-        self.music_menu.play()
+        if self.sound_enabled:
+            self.stop_all()
+            self.music_menu.play()
 
     def playMusicGame(self):
-        self.stop_all()
-        self.music_game.play()
+        if self.sound_enabled:
+            self.stop_all()
+            self.music_game.play()
 
     def playSoundButton(self):
-        self.sound_button.play()
+        if self.sound_enabled:
+            self.sound_button.play()
 
     def playSoundCrash(self):
-        self.sound_crash.play()
+        if self.sound_enabled:
+            self.sound_crash.play()
 
     def playSoundWin(self):
-        self.sound_win.play()
+        if self.sound_enabled:
+            self.sound_win.play()
 
     def playSoundLose(self):
-        self.sound_lose.play()
-
+        if self.sound_enabled:
+            self.sound_lose.play()
 
     def setMusicVolume(self, volume):
         data = struct.pack('f', volume)
         with open("../assets/bin/volume.bin","wb") as f:
             f.write(data)
-        self.music_game.set_volume(volume)
-        self.music_menu.set_volume(volume)
-
-
-
+        if self.sound_enabled:
+            self.music_game.set_volume(volume)
+            self.music_menu.set_volume(volume)
 
     def stop_all(self):
-        pygame.mixer.stop()
-
+        if self.sound_enabled:
+            pygame.mixer.stop()
 
 sound_manager = SoundManager()
