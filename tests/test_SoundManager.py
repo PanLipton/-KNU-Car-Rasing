7a86b2cf385609f0474422077ac632f6d90028f4
TymurWhite <tymurbielykh@gmail.com>
2024-04-15 13:33:38 +0300

    SoundManager tests v3.0 added

--- tests/test_SoundManager.py
+++ tests/test_SoundManager.py
@@ -2,14 +2,21 @@ import sys
 from pathlib import Path
 
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
+# Проверяем наличие Pygame перед импортом SoundManager
+try:
+    import pygame
+except ImportError:
+    pygame = None
 
+# Путь к директории с исходным кодом
 global_dir = Path(__file__).parent.parent / 'src'
 sys.path.append(str(global_dir))
 
-
+# Импорт SoundManager должен происходить после добавления пути в sys.path
 from SoundManager.SoundManager import SoundManager
+
 
 # Фикстура для создания экземпляра SoundManager перед каждым тестом
 @pytest.fixture
@@ -18,13 +25,16 @@ def sound_manager_instance():
     SoundManager._instance = None
     return SoundManager()
 
+
 # Тест проверки синглтона
 def test_singleton_instance(sound_manager_instance):
     assert sound_manager_instance is SoundManager()
 
+
 # Тест инициализации SoundManager
 def test_sound_manager_initialization(sound_manager_instance):
     assert sound_manager_instance.sound_enabled == True
+
 
 # Параметризованный тест для проверки методов воспроизведения звука
 @pytest.mark.parametrize("play_method", [
@@ -32,6 +42,10 @@ def test_sound_manager_initialization(sound_manager_instance):
     ("playMusicGame"),
 ])
 def test_play_methods(sound_manager_instance, mocker, play_method):
+    # Проверяем, что Pygame установлен перед выполнением теста
+    if pygame is None:
+        pytest.skip("Pygame is not installed, skipping test.")
+
     # Создаем мок-объект для замены метода воспроизведения
     mock_play_method = getattr(sound_manager_instance, play_method)
     mock_play_method_mock = MagicMock()
@@ -42,3 +56,15 @@ def test_play_methods(sound_manager_instance, mocker, play_method):
 
     # Проверяем, что вызвался замокированный метод воспроизведения
     mock_play_method_mock.assert_called_once()
+
+
+# Проверяем наличие Pygame перед импортом класса SoundManager
+@patch('SoundManager.SoundManager.pygame', pygame)
+def test_sound_manager_creation():
+    # Проверяем наличие Pygame перед созданием экземпляра SoundManager
+    if pygame is None:
+        pytest.skip("Pygame is not installed, skipping test.")
+
+    # Создаем экземпляр SoundManager
+    sound_manager = SoundManager()
+    assert sound_manager is not None
