13d9c5696a4edaeb13b6fc22da353141331c99d3
TymurWhite <tymurbielykh@gmail.com>
2024-04-16 23:09:41 +0300

    test SoundManager v5.9 added, idea not deleted:(

--- tests/test_SoundManager.py
+++ tests/test_SoundManager.py
@@ -21,26 +21,22 @@ def sound_manager(monkeypatch):
     sound_manager.sound_lose = MagicMock()
     return sound_manager
 
-# Тесты для метода setMusicVolume
 @pytest.mark.parametrize("volume", [0.0, 0.5, 1.0])
 def test_set_music_volume(sound_manager, volume, monkeypatch):
     # Проверяем наличие файла volume.bin
     volume_bin_path = "../assets/bin/volume.bin"
     assert os.path.exists(volume_bin_path), f"File not found: {volume_bin_path}"
 
-    # Мокаем метод set_volume объектов Sound
-    def mock_set_volume(volume):
-        sound_manager.music_game.volume = volume
-        sound_manager.music_menu.volume = volume
-
-    # Применяем мок
-    monkeypatch.setattr(sound_manager.music_game, 'set_volume', mock_set_volume)
-    monkeypatch.setattr(sound_manager.music_menu, 'set_volume', mock_set_volume)
+    # Применяем мок метода set_volume объектов Sound
+    monkeypatch.setattr(sound_manager.music_game, 'set_volume', MagicMock())
+    monkeypatch.setattr(sound_manager.music_menu, 'set_volume', MagicMock())
 
     sound_manager.setMusicVolume(volume)
     # Проверяем, сохраняется ли значение громкости в объектах Sound
-    assert sound_manager.music_game.volume == volume
-    assert sound_manager.music_menu.volume == volume
+    assert sound_manager.music_game.set_volume.call_count == 1
+    assert sound_manager.music_game.set_volume.call_args[0][0] == volume
+    assert sound_manager.music_menu.set_volume.call_count == 1
+    assert sound_manager.music_menu.set_volume.call_args[0][0] == volume
 
 # Тесты для метода playSoundWin
 def test_play_sound_win(sound_manager, monkeypatch):
